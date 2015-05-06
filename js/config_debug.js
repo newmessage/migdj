e713d2a8b62f1f6623c50348c4ce1e93f366c8ad
noobsee <satrio.budidharmawan@gmail.com>
2015-05-06 14:44:14 +0700

    - config debug to server igads

--- js/config_debug.js
+++ js/config_debug.js
@@ -4,7 +4,8 @@
 
 
 const DEBUG                     =   true;
-var SERVER_URL                  =   '';
+var SERVER_URL                  =   'http://ingameads.gameloft.com/ads/adserver/web/fullscreen/templates/minigame/dj/';
+
 var deviceCountry               =   'EN';
 var creative_id                 =   '';
 var creative_type_id 			= 	'27';
